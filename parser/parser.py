ac20a825c2f4f5273999222d1e0fdbd62d7e71dc
gehariharan <gehariharan@users.noreply.github.com>
2023-01-31 18:37:27 -0800

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -1,4 +1,5 @@
-#python 3.x
+#python code to parse and use json
+
 import json
 data = json.loads('{"one" : "1", "two" : "2", "three" : "3"}')
 print(data['two']) # or `print data['two']` in Python 2
