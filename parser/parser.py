8c468a3608bbcb767cf3bbfb67489f854f43aec0
gehariharan <gehariharan@users.noreply.github.com>
2023-01-31 18:47:06 -0800

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -1,4 +1,31 @@
-#python code to parse and use json
+
+#Parse and use json in python 3.x
 import json
 data = json.loads('{"one" : "1", "two" : "2", "three" : "3"}')
-print(data['two']) # or `print data['two']` in Python 2
+print(data['two']) # or `print data['two']` in Python 2 
+
+
+
+#From JSON file
+f = open('data.json',)
+data = json.load(f)
+for i in data['two']:
+    print(i)
+f.close() # Write JSON string to file
+data = {
+'employees' : [
+    {
+        'name' : 'John Doe',
+        'department' : 'Marketing',
+        'place' : 'Remote'
+    },
+    {
+        'name' : 'Jane Doe',
+        'department' : 'Software Engineering',
+        'place' : 'Remote'
+    }
+]
+} json_string = json.dumps(data)
+# Using a JSON string
+with open('json_data.json', 'w') as outfile:
+    outfile.write(json_string)
